145faa3ce98d7a6fe836a1b233d91556369d311c
NurUddin111 <nuruddinmuhammad38@gmail.com>
2026-02-26 19:00:51 +0600

    login & logout function added

--- backend/src/app/routes/auth.route.ts
+++ backend/src/app/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller";
+import { login, logout, signup } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export const AuthRoutes = router;
