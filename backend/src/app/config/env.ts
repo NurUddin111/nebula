a83138488c854e92629929d86144b3ecdc6e34ce
NurUddin111 <nuruddinmuhammad38@gmail.com>
2026-02-26 11:52:21 +0600

    werlcome email implementend

--- backend/src/app/config/env.ts
+++ backend/src/app/config/env.ts
@@ -8,6 +8,10 @@ interface IEnvConfig {
   NODE_ENV: "development" | "production";
   JWT_ACCESS_SECRET: string;
   JWT_ACCESS_EXPIRES: string;
+  RESEND_API_KEY: string;
+  EMAIL_FROM: string;
+  EMAIL_FROM_NAME: string;
+  CLIENT_URL: string;
 }
 
 const loadEnvVariables = (): IEnvConfig => {
@@ -17,6 +21,10 @@ const loadEnvVariables = (): IEnvConfig => {
     "NODE_ENV",
     "JWT_ACCESS_SECRET",
     "JWT_ACCESS_EXPIRES",
+    "RESEND_API_KEY",
+    "EMAIL_FROM",
+    "EMAIL_FROM_NAME",
+    "CLIENT_URL",
   ];
 
   requiredEnvVars.forEach((key) => {
@@ -31,6 +39,10 @@ const loadEnvVariables = (): IEnvConfig => {
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    RESEND_API_KEY: process.env.RESEND_API_KEY as string,
+    EMAIL_FROM: process.env.EMAIL_FROM as string,
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME as string,
+    CLIENT_URL: process.env.CLIENT_URL as string,
   };
 };
 
