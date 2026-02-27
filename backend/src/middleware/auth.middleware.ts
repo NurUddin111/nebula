273cd62b771802bbff148f75b812b328c5b4ac8d
NurUddin111 <nuruddinmuhammad38@gmail.com>
2026-02-28 02:03:00 +0600

    update-profile functionality added

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import User from "../app/models/User";
+import { envVars } from "../app/config/env";
+
+export const protectRoute = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No token provided" });
+
+    const decoded = jwt.verify(token, envVars.JWT_ACCESS_SECRET) as JwtPayload;
+    if (!decoded)
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
